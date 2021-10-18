56c6cba87133cbc7c7b444f6581fd1a23a734d62
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-10-18 16:16:34 -0300

    Utilizando o js externo

---
+++ Projetos/03_01_UtilizandoJSExterno/script.js
@@ -0,0 +1,9 @@
+var pessoaVelha = prompt("Nome da pessoa mais velha: ")
+var idadePessoaVelha = prompt("Idade da pessoa mais velha: ")
+var pessoaNova = prompt("Nome da pessoa mais nova: ")
+var idadePessoaNova = prompt("Idade da pessoa mais nova: ")
+
+var diferencaIdade = idadePessoaVelha - idadePessoaNova;
+
+alert('A pessoa mais velha é ' + pessoaVelha + " e sua idade é " + idadePessoaVelha + " . \n\nA pessoa mais nova é " + pessoaNova + " e sua idade é "+ idadePessoaNova + ".\n\nA diferena de idade dos dois é " + diferencaIdade);
+lucasluca

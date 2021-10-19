e70620ac118dfdb2977e1c20a99a79e85e38a31d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-10-19 13:14:47 -0300

    switch em js

---
+++ Semana 03 - Javascript/09_switch.js
@@ -0,0 +1,16 @@
+let spaceship = "Elemental"
+
+switch(spaceship){
+    case "Golias":
+        console.log("Nave mais resistente.")
+        //executa todo o código até encontrar um break.
+        break;
+    case "Rator":
+        console.log("Nave mais rápida.")
+        break;
+    case "Enterprise":
+        console.log("Nave a Frota Estelar.")
+        break;
+    default:
+        console.log("Nave comum.")
+}

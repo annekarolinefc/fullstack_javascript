68880136cdf1b3d15b330675acdef6e31f06d974
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-10-20 19:04:01 -0300

    Break com JS

---
+++ Semana03/Treinando _Javascript/14_break.js
@@ -0,0 +1,8 @@
+let spaceshipName = "Supernova"
+
+for(let i=0; i< spaceshipName.length; i++){
+    if(spaceshipName[i] =="o"){
+        break; //o for para completamente e sai de execução.
+    }
+    console.log(spaceshipName[i])
+}

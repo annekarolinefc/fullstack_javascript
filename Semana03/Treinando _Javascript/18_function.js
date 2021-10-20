7367f77421a56a9adb6e491ac37167c01ca1a78c
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-10-20 19:04:10 -0300

    Function com JS

---
+++ Semana03/Treinando _Javascript/18_function.js
@@ -0,0 +1,12 @@
+function soma(n1, n2, n3, n4){
+    return n1+n2+n3+n4;
+}
+let somaNumeros= soma(1, 1, 1, 1)
+
+console.log(somaNumeros)
+
+function media(n1, n2, n3, n4){
+    return (n1+n2+n3+n4)/4
+}
+let mediaEscolar = media(20, 12, 16, 25);
+console.log(mediaEscolar)

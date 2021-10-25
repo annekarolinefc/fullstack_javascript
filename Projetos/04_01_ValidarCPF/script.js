20e872bf9d5e735a6e552b9f949a7f692bfd0d4d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-10-25 13:33:22 -0300

    Validar CPF

---
+++ Projetos/04_01_ValidarCPF/script.js
@@ -0,0 +1,37 @@
+//composto por 11 dígitos
+//9 primeiros -> numeros base
+//2 ultimos -> digitos de verificação
+
+
+//calculo 
+//111.222.333-xx
+//1 * 10 = 10
+//1 * 9 = 9
+//1 * 8 = 8
+//2 * 7 = 14
+//2 * 6 = 12
+//2 * 5 = 10
+//3 * 4 = 12
+//3 * 3 = 9
+//3 * 2 = 6
+//soma = 90 -> 90/11 = 8 e sobra 2 -> dv = 11-2 - 9
+//primeiro digito = 9;
+
+//mudando 
+//1 * 11 = 11
+//1 * 10 = 10
+//1 * 9 = 9
+//2 * 8 = 16
+//2 * 7 = 14
+//2 * 6 = 12
+//3 * 5 = 15
+//3 * 4 = 12
+//3 * 3 = 9
+//9 * 2 = 19
+//soma = 126
+//126/11 = 11 e sobra 5 ->  dv = 11-5 = 6
+//segundo numero = 6
+
+
+
+

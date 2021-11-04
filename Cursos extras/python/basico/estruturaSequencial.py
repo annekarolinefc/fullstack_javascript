9b7a60d24433c68e1599f961a166a27dc8e153ce
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-04 19:04:39 -0300

    Estrutura sequencial

---
+++ Cursos extras/python/basico/estruturaSequencial.py
@@ -0,0 +1,14 @@
+#programa é executado linha a linha
+idade = input("Informe a sua idade: ")
+print(idade, type(idade))
+
+#transformar tipo
+idade = int(idade)
+
+
+# Entrada de dados: input
+# Saída de dados: output
+
+#Tudo que é lido por input() é considerado uma string(str)
+peso = float(input('Digite o seu peso: '))
+idade = int(input('Digite a sua idade: '))

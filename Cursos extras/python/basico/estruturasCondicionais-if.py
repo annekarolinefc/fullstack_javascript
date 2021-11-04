79e29d7e5033c2f3f9baa638812565ec70c400da
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-04 19:05:04 -0300

    Condicional - if Python

---
+++ Cursos extras/python/basico/estruturasCondicionais-if.py
@@ -0,0 +1,10 @@
+#IF
+idade = 20 
+if idade >= 12:
+    print("Você pode entrar na montanha russa.")
+print("Obrigado por participar.")
+
+altura = 1.56
+if idade >=12 and altura >=1.60:
+    print("Você pode entrar na montanha russa.")
+print("Obrigado por participar")

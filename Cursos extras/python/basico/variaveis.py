4aa2a59dcbc2dad0c161894b2db8864754328596
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-04 19:04:30 -0300

    variáveis -python

---
+++ Cursos extras/python/basico/variaveis.py
@@ -0,0 +1,21 @@
+#inteiros
+x = 5
+print(x)
+#mostrar tipo da variável
+print(type(x))
+
+#float
+preco = 19.99
+print(preco)
+print(type(preco))
+
+#String
+cidade="Belo Horizonte"
+print(cidade)
+print(type(cidade))
+
+#Boolean - T ou F
+disponivel = True
+print(disponivel)
+print(type(disponivel))
+

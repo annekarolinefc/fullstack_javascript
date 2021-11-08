076470a05283dfde16012f9e178482f406ee0d21
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-08 15:59:08 -0300

    EXPORT DEFAULT

---
+++ Semana05/04-export-default/src/spaceship.js
@@ -0,0 +1,24 @@
+class Spaceship{
+    constructor(name, captain, armaments=[], defenses=[]){
+        this.name = name;
+        this.captain = captain;
+        this.armaments = armaments;
+        this.defenses = defenses;
+    }
+}
+
+
+export default Spaceship
+
+/*
+OU
+
+export default class Spaceship{
+    constructor(name, captain, armaments=[], defenses=[]){
+        this.name = name;
+        this.captain = captain;
+        this.armaments = armaments;
+        this.defenses = defenses;
+    }
+}
+*/

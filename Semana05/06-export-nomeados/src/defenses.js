7dc41828a12c7abfcaad44c812f1cab24a8b3438
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-09 14:21:14 -0300

    Export Nomeados

---
+++ Semana05/06-export-nomeados/src/defenses.js
@@ -0,0 +1,7 @@
+const laserDefenses = ["Escudos Defletores", "Arma anti-laser"]
+const steelDefenses = ["Escudo de Aço"]
+
+//EXPORT NOMEADO
+export {laserDefenses, steelDefenses}
+
+export default ["Escudo Repulsar"]

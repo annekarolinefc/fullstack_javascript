d31e3f761f4a51e799218cbf3c3852018542de58
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-09 16:03:19 -0300

    promisses

---
+++ Treinando _Javascript/36_setTimeout.js
@@ -0,0 +1,3 @@
+setTimeout(()=>{
+    console.log("Executando após 2 segundos")
+}, 2000) //executa após 2.000 milisegundos
---
+++ Treinando _Javascript/37_setInterval.js
@@ -0,0 +1,3 @@
+setInterval(()=>{
+    console.log("Executando a cada 2 segundos")
+}, 2000) //executa a cada 2.000 milisegundos

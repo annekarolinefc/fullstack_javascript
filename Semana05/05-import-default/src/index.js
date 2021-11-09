256847cac241d1942472c954254fdadaa851d4b7
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-11-09 14:20:59 -0300

    Import Default

---
+++ Semana05/05-import-default/src/index.js
@@ -0,0 +1,7 @@
+import Spaceship from "./spaceship";
+import armaments from "./armaments";
+
+
+const spaceship = new Spaceship("USS Enterprise", "James Tiberius Kirk", armaments.laser)
+
+console.log(spaceship)
